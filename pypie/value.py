20dcfa2484fd500c2df3ff8303c30e2fc7f9c56f
Martin Billinger <flkazemakase@gmail.com>
2021-12-03 11:20:48 +0100

    document the Value API

--- pypie/value.py
+++ pypie/value.py
@@ -6,13 +6,18 @@ from pypie import Ctx, Expr, Env, quote, expr
 
 
 class Value(ABC):
+    """Values are the results of evaluating expressions."""
+
     def read_back_type(self, ctx: Ctx) -> Expr:
+        """Convert a type-value back to an expression."""
         raise NotImplementedError(f"{self.__class__.__name__}.read_back_type()")
 
     def read_back(self, val: "Value", ctx: Ctx) -> Expr:
+        """Assuming self is a type-value, convert a value of that type back to an expression."""
         raise NotImplementedError(f"{self.__class__.__name__}.read_back({val})")
 
     def now(self) -> "Value":
+        """Return the actual value, computing it if necessary."""
         return self
 
 
