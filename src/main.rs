736a08ce6fc11ceee3eacaa9aebbab997023dbfd
Martin Billinger <mbillingr@users.noreply.github.com>
2022-01-01 20:17:59 +0100

    support claim/define in the REPL

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use the_little_typer as tlt;
 
+use sexpr_parser::parse;
 use std::{io, io::Write};
+use the_little_typer::sexpr::Sexpr;
 use tlt::{
     basics::{Core, Ctx},
     rep::norm,
@@ -8,16 +10,45 @@ use tlt::{
 };
 
 fn main() -> io::Result<()> {
-    let ctx = Ctx::new();
+    let mut ctx = Ctx::new();
     loop {
-        let src = read_line()?;
-        let core: Core = src.parse().unwrap();
-
-        match norm(&ctx, &core) {
-            Ok(out) => println!("{}", resugar(&out)),
+        match read_eval_normalize(&mut ctx) {
+            Ok(None) => {}
+            Ok(Some(out)) => println!("{}", resugar(&out)),
             Err(e) => eprintln!("{}", e),
         }
     }
+}
+
+fn read_eval_normalize(ctx: &mut Ctx) -> Result<Option<Core>, String> {
+    let src = read_line().map_err(|e| e.to_string())?;
+    let sexpr = parse::<Sexpr>(&src).map_err(|e| format!("{:?}", e))?;
+    match &sexpr {
+        Sexpr::List(list) => match &list[..] {
+            [Sexpr::Symbol(form), Sexpr::Symbol(ident), val] => {
+                match (form.name(), ident.name(), val) {
+                    ("claim", _, expr) => {
+                        *ctx = ctx
+                            .claim(ident.clone(), expr.into())
+                            .map_err(|e| e.to_string())?;
+                        return Ok(None);
+                    }
+                    ("define", _, expr) => {
+                        *ctx = ctx
+                            .define(ident.clone(), expr.into())
+                            .map_err(|e| e.to_string())?;
+                        return Ok(None);
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+        },
+        _ => {}
+    }
+    norm(ctx, &(&sexpr).into())
+        .map(Some)
+        .map_err(|e| e.to_string())
 }
 
 fn read_line() -> io::Result<String> {
