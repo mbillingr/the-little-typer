3e18771b657d4413411adb0de54ae95411ab69f9
Martin Billinger <mbillingr@users.noreply.github.com>
2022-01-08 12:37:58 +0100

    define first

--- src/book/chapter_06.rs
+++ src/book/chapter_06.rs
@@ -3,6 +3,9 @@ use crate::book::{with_empty_context, Checker, ResultBoolAssertions};
 
 fn with_chapter_context() -> Checker {
     with_book_context()
+        .claim("first", "(Π ((E U) (l Nat)) (-> (Vec E (add1 l)) E))")
+        .define("first", "(λ (E l v) (head v))")
+        .unwrap()
 }
 
 #[test]
@@ -52,5 +55,14 @@ fn test_24_tail() {
         .core("(tail (the (Vec Atom 1) (vec:: 'a vecnil)))")
         .and("vecnil")
         .are_the_same("(Vec Atom 0)")
+        .assert(true);
+}
+
+#[test]
+fn test_36_first() {
+    with_chapter_context()
+        .core("(first Atom 3 (vec:: 'chicken-of-the-woods (vec:: 'chantrelle (vec:: 'lions-mane (vec:: 'puffball vecnil)))))")
+        .and("'chicken-of-the-woods")
+        .are_the_same("Atom")
         .assert(true);
 }
