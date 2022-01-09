2ed63a532a268ddd45e2ee145e55bca167351d07
Martin Billinger <mbillingr@users.noreply.github.com>
2022-01-09 15:46:47 +0100

    define drop-last

--- src/book/chapter_07.rs
+++ src/book/chapter_07.rs
@@ -3,6 +3,9 @@ use crate::book::{with_empty_context, Checker, ResultBoolAssertions};
 
 fn with_chapter_context() -> Checker {
     with_book_context()
+        // ------
+        //  last
+        // ------
         .claim("last", "(Π ((E U) (l Nat)) (-> (Vec E (add1 l)) E))")
         .claim("base-last", "(Π ((E U)) (-> (Vec E 1) E))")
         .define("base-last", "(λ (E) (λ (es) (head es)))")
@@ -22,6 +25,33 @@ fn with_chapter_context() -> Checker {
         .define(
             "last",
             "(λ (E l) (ind-Nat l (mot-last E) (base-last E) (step-last E)))",
+        )
+        .unwrap()
+        // -----------
+        //  drop-last
+        // -----------
+        .claim(
+            "drop-last",
+            "(Π ((E U) (l Nat)) (-> (Vec E (add1 l)) (Vec E l)))",
+        )
+        .claim("base-drop-last", "(Π ((E U)) (-> (Vec E 1) (Vec E 0)))")
+        .define("base-drop-last", "(λ (E es) vecnil)")
+        .unwrap()
+        .claim("mot-drop-last", "(-> U Nat U)")
+        .define("mot-drop-last", "(λ (E k) (-> (Vec E (add1 k)) (Vec E k)))")
+        .unwrap()
+        .claim(
+            "step-drop-last",
+            "(Π ((E U) (l-1 Nat)) (-> (mot-drop-last E l-1) (mot-drop-last E (add1 l-1))))",
+        )
+        .define(
+            "step-drop-last",
+            "(λ (E l-1) (λ (drop-last_l-1) (λ (es) (vec:: (head es) (drop-last_l-1 (tail es))))))",
+        )
+        .unwrap()
+        .define(
+            "drop-last",
+            "(λ (E l) (ind-Nat l (mot-drop-last E) (base-drop-last E) (step-drop-last E)))",
         )
         .unwrap()
 }
@@ -74,4 +104,72 @@ fn test_55_56_also_rec_nat() {
         .and("'celery")
         .are_the_same("Atom")
         .assert(true);
+}
+
+/* Frames 70 - 72 performed manually:
+    0. | (drop-last Atom 2)
+    1. | (drop-last Atom (add1 (add1 zero)))
+    2. | (ind-Nat (add1 (add1 zero))
+       |   (mot-drop-last Atom)
+       |   (base-drop-last Atom)
+       |   (step-drop-last Atom))
+    3. | (step-drop-last Atom (add1 zero)
+       |   (ind-Nat (add1 zero)
+       |     (mot-drop-last Atom)
+       |     (base-drop-last Atom)
+       |     (step-drop-last Atom)))
+    4. | (λ (es)
+       |    (vec:: (head es)
+       |           ((ind-Nat (add1 zero)
+       |              (mot-drop-last Atom)
+       |              (base-drop-last Atom)
+       |              (step-drop-last Atom))
+       |            (tail es))))
+
+    5. | (λ (es)
+       |    (vec:: (head es)
+       |           ((step-drop-last Atom zero
+       |              (ind-Nat zero
+       |                (mot-drop-last Atom)
+       |                (base-drop-last Atom)
+       |                (step-drop-last Atom)))
+       |            (tail es))))
+    6. | (λ (es)
+       |    (vec:: (head es)
+       |           ((λ (es)
+       |               (vec:: (head es)
+       |                      ((ind-Nat zero
+       |                         (mot-drop-last Atom)
+       |                         (base-drop-last Atom)
+       |                         (step-drop-last Atom))
+       |                       (tail es))))
+       |            (tail es))))
+    7. | (λ (es)
+       |    (vec:: (head es)
+       |           (vec:: (head (tail es))
+       |                  ((ind-Nat zero
+       |                     (mot-drop-last Atom)
+       |                     (base-drop-last Atom)
+       |                     (step-drop-last Atom))
+       |                   (tail (tail es)))))
+       |            )))
+    8. | (λ (es)
+       |    (vec:: (head es)
+       |           (vec:: (head (tail es))
+       |                  ((base-drop-last Atom)
+       |                   (tail (tail es)))))
+       |            )))
+    9. | (λ (es)
+       |    (vec:: (head es)
+       |           (vec:: (head (tail es))
+       |                  vecnil)))
+*/
+
+#[test]
+fn test_70_27_drop_last() {
+    with_chapter_context()
+        .core("(drop-last Atom 2)")
+        .and("(λ (es) (vec:: (head es) (vec:: (head (tail es)) vecnil)))")
+        .are_the_same("(-> (Vec Atom 3) (Vec Atom 2))")
+        .assert(true)
 }
