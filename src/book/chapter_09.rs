f7b7406ac04b6e8dbec19b9c1452deecabd97454
Martin Billinger <flkazemakase@gmail.com>
2022-01-13 21:57:01 +0100

    proved that twice equals double

--- src/book/chapter_09.rs
+++ src/book/chapter_09.rs
@@ -1,5 +1,5 @@
 use crate::book::common_definitions::with_book_context;
-use crate::book::{with_empty_context, Checker, ResultAssertions, ResultBoolAssertions};
+use crate::book::{Checker, ResultBoolAssertions};
 
 fn with_chapter_context() -> Checker {
     with_book_context()
@@ -14,6 +14,12 @@ fn with_chapter_context() -> Checker {
         // --------
         .claim("double", "(-> Nat Nat)")
         .define("double", "(λ (n) (iter-Nat n 0 (+ 2)))")
+        .unwrap()
+        // -------
+        //  twice
+        // -------
+        .claim("twice", "(-> Nat Nat)")
+        .define("twice", "(λ (n) (+ n n))")
         .unwrap()
 }
 
@@ -51,5 +57,82 @@ fn frame_19_use_replace_instead_of_cong() {
     ctx.core("(incr=add1 2)")
         .and("(same 3)")
         .are_the_same("(= Nat 3 3)")
+        .assert(true);
+}
+
+#[test]
+fn frame_22_54_prove_that_twice_equals_double() {
+    let ctx = with_chapter_context()
+        .claim(
+            "twice=double",
+            "(Π ((n Nat))
+                (= Nat
+                   (twice n)
+                   (double n)))",
+        )
+        .claim(
+            "add1+=+add1",
+            "(Π ((n Nat) (j Nat))
+                (= Nat
+                   (add1 (+ n j))
+                   (+ n (add1 j))))",
+        )
+        .claim("mot-add1+=+add1", "(-> Nat Nat U)")
+        .define(
+            "mot-add1+=+add1",
+            "(λ (j k) (= Nat (add1 (+ k j)) (+ k (add1 j))))",
+        )
+        .unwrap()
+        .claim(
+            "step-add1+=+add1",
+            "(Π ((j Nat) (n-1 Nat)) (-> (mot-add1+=+add1 j n-1) (mot-add1+=+add1 j (add1 n-1))))",
+        )
+        .define(
+            "step-add1+=+add1",
+            "(λ (j n-1 add1+=+add1_n-1) (cong add1+=+add1_n-1 (+ 1)))",
+        )
+        .unwrap()
+        .define(
+            "add1+=+add1",
+            "(λ (n j) (ind-Nat n (mot-add1+=+add1 j) (same (add1 j)) (step-add1+=+add1 j)))",
+        )
+        .unwrap()
+        .claim("mot-twice=double", "(-> Nat U)")
+        .define("mot-twice=double", "(λ (k) (= Nat (twice k) (double k)))")
+        .unwrap()
+        .claim(
+            "step-twice=double",
+            "(Π ((n-1 Nat)) (-> (mot-twice=double n-1) (mot-twice=double (add1 n-1))))",
+        )
+        .claim(
+            "mot-step-twice=double",
+            "(-> Nat Nat U)",
+        )
+        .define(
+            "mot-step-twice=double",
+            "(λ (n-1 k) (= Nat (add1 k) (add1 (add1 (double n-1)))))",
+        ).unwrap()
+        .define(
+            "step-twice=double",
+            "(λ (n-1 twice=double_n-1) (replace (add1+=+add1 n-1 n-1) (mot-step-twice=double n-1) (cong twice=double_n-1 (+ 2))))",
+        )
+        .unwrap()
+        .define(
+            "twice=double",
+            "(λ (n) (ind-Nat n mot-twice=double (same zero) step-twice=double))",
+        )
+        .unwrap();
+
+    let ctx = ctx
+        .claim("twice=double-of-17", "(= Nat (twice 17) (double 17))")
+        .claim("twice=double-of-17-again", "(= Nat (twice 17) (double 17))")
+        .define("twice=double-of-17", "(twice=double 17)")
+        .unwrap()
+        .define("twice=double-of-17-again", "(same 34)")
+        .unwrap();
+
+    ctx.core("(twice=double 17)")
+        .and("(same 34)")
+        .are_the_same("(= Nat (twice 17) (double 17))")
         .assert(true);
 }
