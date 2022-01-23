507c56003d846d46fd1976eeef083c6765fe2605
Martin Billinger <mbillingr@users.noreply.github.com>
2022-01-23 16:39:09 +0100

    chapter 11

--- src/book/chapter_11.rs
+++ src/book/chapter_11.rs
@@ -87,14 +87,78 @@ fn frame_27_vec_append() {
 }
 
 #[test]
-fn frame_35_claim_for_the_external_proof() {
-    let _ctx = with_chapter_context().claim(
-        "list->vec->list",
-        "(Π ((E U) (es (List E)))
+fn frame_35_55_the_external_proof() {
+    let _ctx = with_chapter_context()
+        .claim(
+            "list->vec->list=",
+            "(Π ((E U) (es (List E)))
             (= (List E)
                es
                (vec->list E
                    (length E es)
                    (list->vec E es))))",
-    );
+        )
+        .claim("mot-list->vec->list=", "(Π ((E U)) (-> (List E) U))")
+        .define(
+            "mot-list->vec->list=",
+            "(λ (E es) (= (List E) es (vec->list E (length E es) (list->vec E es))))",
+        )
+        .unwrap()
+        .claim(
+            "step-list->vec->list=",
+            "(Π ((E U) (e E) (es (List E)))
+                (-> (mot-list->vec->list= E es) (mot-list->vec->list= E (:: e es))))",
+        )
+        .claim("::-fun", "(Π ((E U)) (-> E (List E) (List E)))")
+        .define("::-fun", "(λ (E e es) (:: e es))")
+        .unwrap()
+        .define(
+            "step-list->vec->list=",
+            "(λ (E e es list->vec->list=_es) (cong list->vec->list=_es (::-fun E e)))",
+        )
+        .unwrap()
+        .define("list->vec->list=", "(λ (E es) (ind-List es (mot-list->vec->list= E) (same nil) (step-list->vec->list= E)))")
+        .unwrap();
+}
+
+#[test]
+fn frame_45_46_proving_the_treat_statement() {
+    let _ctx = with_chapter_context()
+        .claim("Treat-Statement", "U")
+        .define(
+            "Treat-Statement",
+            "(Π ((some-treats (List Atom))
+                 (more-treats (List Atom)))
+                (-> (= (List Atom) some-treats more-treats)
+                    (= (List Atom) (:: 'plättar some-treats) (:: 'plättar more-treats))))",
+        )
+        .unwrap()
+        .claim("::-plättar", "(-> (List Atom) (List Atom))")
+        .define(
+            "::-plättar",
+            "(λ (tasty-treats) (:: 'plättar tasty-treats))",
+        )
+        .unwrap()
+        .claim("treat-proof", "Treat-Statement")
+        .define(
+            "treat-proof",
+            "(λ (some-treats more-treats treats=) (cong treats= ::-plättar))",
+        );
+}
+
+#[test]
+fn frame_48_proving_two_equal_lists_have_equal_length() {
+    let _ctx = with_chapter_context()
+        .claim(
+            "length-treats=",
+            "(Π ((some-treats (List Atom))
+                 (more-treats (List Atom)))
+                (-> (= (List Atom) some-treats more-treats)
+                    (= Nat (length Atom some-treats) (length Atom more-treats))))",
+        )
+        .define(
+            "length-treats=",
+            "(λ (some-treats more-treats treats=) (cong treats= (length Atom)))",
+        )
+        .unwrap();
 }
