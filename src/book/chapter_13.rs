59eface2579f50b10ae6971edac8310023ff1690
Martin Billinger <mbillingr@users.noreply.github.com>
2022-01-27 00:04:22 +0100

    starting chapter 13

--- src/book/chapter_13.rs
+++ src/book/chapter_13.rs
@@ -2,7 +2,25 @@ use crate::book::{chapter_12, Checker, ResultBoolAssertions};
 use lazy_static::lazy_static;
 
 lazy_static! {
-    static ref CHAPTER_CONTEXT: Checker = chapter_12::with_chapter_context();
+    static ref CHAPTER_CONTEXT: Checker = chapter_12::with_chapter_context()
+        // -------------
+        //  even-or-odd
+        // -------------
+        .claim("even-or-odd", "(Π ((n Nat)) (Either (Even n) (Odd n)))")
+        .claim("mot-even-or-odd", "(-> Nat U)")
+        .define("mot-even-or-odd", "(λ (k) (Either (Even k) (Odd k)))")
+        .unwrap()
+        .claim("step-even-or-odd", "(Π ((n-1 Nat)) (-> (mot-even-or-odd n-1) (mot-even-or-odd (add1 n-1))))")
+        .define(
+            "step-even-or-odd",
+            "(λ (n-1 e-or-o_n-1)
+                (ind-Either e-or-o_n-1
+                    (λ (e-or-o)
+                       (mot-even-or-odd (add1 n-1)))
+                    (λ (e_n-1) (right (add1-even->odd n-1 e_n1)))
+                    (λ (o_n-1) (left (add1-odd->even n-1 o_n1)))))")
+        .unwrap()
+        ;
 }
 
 pub fn with_chapter_context() -> Checker {
